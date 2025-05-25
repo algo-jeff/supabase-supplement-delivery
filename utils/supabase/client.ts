0b47d627b06241145a30b14de717046e094bcada
algo-jeff <jeff@algocarelab.com>
2025-05-25 20:29:41 +0900

    Add server-side Supabase utilities

---
+++ utils/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from '@supabase/supabase-js'
+
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
